f6a74aea16422b396aef3fc0cab0f226ac9b0c89
Joakim R. S. <jose0001@stud.kea.dk>
2025-03-31 13:04:31 +0200

    Tilføjet ProfileRowMapper

---
+++ src/main/java/com/example/wishlist/rowMapper/ProfileRowMapper.java
@@ -0,0 +1,28 @@
+package com.example.wishlist.rowMapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import com.example.wishlist.model.Profile;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.*;
+
+public class ProfileRowMapper implements RowMapper<Profile> {
+
+    @Override
+    public Profile mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Profile profile = new Profile();
+        profile.setId(rs.getInt("id"));
+        profile.setName(rs.getString("name"));
+        profile.setBirthday(LocalDate.of(                                  // Opretter nyt LocalDate-objekt
+                Integer.parseInt(rs.getString("birthday").split("-")[0]), // Splitter result settet og tager årstallet
+                Integer.parseInt(rs.getString("birthday").split("-")[1]), // Splitter result settet og tager måneden
+                Integer.parseInt(rs.getString("birthday").split("-")[2])) // Splitter result settet og tager dagen
+        );
+        profile.setEmail(rs.getString("email"));
+        profile.setUserName(rs.getString("user_name")); // Midlertidig indtil vi får sat vores environment variables op
+        profile.setPassword(rs.getString("password")); // Midlertidig indtil vi får sat vores environment variables op
+        return profile;
+    }
+}
