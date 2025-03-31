d383610cef465a7c5a29b1db5e01183e9e030d37
Sofie Broman <sofierytter@hotmail.com>
2025-03-31 10:40:16 +0200

    .

--- src/main/java/com/example/wishlist/repository/IRepository.java
+++ src/main/java/com/example/wishlist/repository/IRepository.java
@@ -1,6 +1,19 @@
 package com.example.wishlist.repository;
 
+import java.util.*;
+
 public interface IRepository<T, Integer> {
 
+    public boolean save(T t);
+
+    public boolean create(T t);
+
+    public T findById(Integer id);
+
+    public List<T> findAll();
+
+    public boolean deleteById(Integer id);
+
+    public boolean update(T t);
 
 }
--- src/main/java/com/example/wishlist/repository/ProfileRepository.java
+++ src/main/java/com/example/wishlist/repository/ProfileRepository.java
@@ -1,16 +1,42 @@
 package com.example.wishlist.repository;
 
-
+import com.example.wishlist.model.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 
-public class ProfileRepository {
+public class ProfileRepository implements IRepository{
 
     private final JdbcTemplate jdbcTemplate;
 
     public ProfileRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public boolean save(Profile profile) {
+
+    }
+
+    public boolean create(Profile profile) {
+
+    }
+
+    public Profile findById(int id) {
+
+    }
+
+    public List<Profile> findAll() {
+
+    }
+
+    public boolean deleteById(Integer id) {
+
+    }
+
+    public boolean update(T t) {
+
     }
 }
