a533d63e361aa139bcd9dfbecc8cfef33ff39be2
Joakim R. S. <jose0001@stud.kea.dk>
2025-04-03 17:46:46 +0200

    SHop og lav tricks på den

--- src/main/java/com/example/wishlist/repository/IProfileRepository.java
+++ src/main/java/com/example/wishlist/repository/IProfileRepository.java
@@ -7,10 +7,18 @@ import java.util.List;
 
 public interface IProfileRepository extends IRepository<Profile, Integer> {
     void save(Profile t);
+
     void create(Profile t);
+
     Profile findById(Integer id);
+
     List<Profile> findAll();
+
     void deleteById(Integer id);
+
     void update(Profile t);
 
+    Profile findProfileByUserName(String username);
+
+    void editProfile(Profile uneditedProfile, Profile editedProfile);
 }
--- src/main/java/com/example/wishlist/service/IProfileService.java
+++ src/main/java/com/example/wishlist/service/IProfileService.java
@@ -12,9 +12,21 @@ public interface IProfileService extends IService<Profile, Integer> {
 
     Profile findById(Integer id);
 
+    Profile findByUserName(String userName);
+
     List<Profile> findAll();
 
     void deleteById(Integer id);
 
     void update(Profile profile);
+
+    Profile findProfileByUserName(String username);
+
+    void editProfile(Profile uneditedProfile, Profile editedProfile);
+
+    void createProfile(Profile profile);
+
+    boolean profileAlreadyExists(String username);
+
+    boolean login(String username, String password);
 }
